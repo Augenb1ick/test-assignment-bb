88804fe5527b0a2f2ab821a635ad2adbe89a3c5f
Augenb1ick <augenblick14@yandex.ru>
2023-09-29 15:36:31 +0400

    fix: unknown route

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ const App = () => {
             />
           }
         />
-        <Route path='*' element={<Navigate to={<Home />} />} />
+        <Route path='*' element={<Navigate to='/' replace={true} />} />
       </Routes>
     </>
   );
